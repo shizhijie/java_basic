c9f5536eb7aeb8a75b916dd8e2d9e68ccd9e867d
shizhijie <2272684702@qq.com>
2019-06-03 11:43:02 +0800

    一元加减操作符

---
+++ src/com/zjs/bwcx/一元加减操作符/AddAndSubtract.java
@@ -0,0 +1,13 @@
+package com.zjs.bwcx.一元加减操作符;
+
+public class AddAndSubtract {
+	public static void main(String[] args) {
+		int x = 4,a = 5,b = 3;
+		x = -a;
+		System.out.println(x);
+		x = a * -b;
+		System.out.println(x);
+		x = a * (-b);
+		System.out.println(x);
+	}
+}
